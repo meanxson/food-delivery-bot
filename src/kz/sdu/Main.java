35f667af2c003f4d1fd5750f1a233a9ebdd94b95
Sanzhar <semid46@gmail.com>
2021-12-01 19:22:50 +0600

    init

---
+++ src/kz/sdu/Main.java
@@ -0,0 +1,7 @@
+package kz.sdu;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/kz/sdu/bot/TelegramBot.java
@@ -0,0 +1,21 @@
+package kz.sdu.bot;
+
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+public class TelegramBot extends TelegramLongPollingBot {
+    @Override
+    public String getBotUsername() {
+        return "fooood_delivery_bot";
+    }
+
+    @Override
+    public String getBotToken() {
+        return "2130702439:AAFDouGPGU90JyB--82859SL7rU3DeprNQo";
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+
+    }
+}
